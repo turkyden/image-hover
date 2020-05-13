161e35d18b11dd3497f4f754d65502f12218100e
Turkyden <wj871287@gmail.com>
2020-05-13 20:30:11 +0800

    Added CI

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  configureWebpack: {
+    output: {
+      libraryExport: 'default'
+    }
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/vue-imagehover/'
+  : '/'
+}
