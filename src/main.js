e3c92d2c88e2f729c3423b00c8fbadf55ebb76f1
Turkyden <wj871287@gmail.com>
2020-05-22 17:30:56 +0800

    Fixed Hightlight Theme

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueHighlightJS from 'vue-highlightjs'
 import VueClipboard from 'vue-clipboard2'
-import './assets/night-owl.css'
+import './assets/atom-one-dark.css'
 
 // VueClipboard
 VueClipboard.config.autoSetContainer = true // add this line
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   theme: {},
   variants: {},
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/ui'),
+  ]
 }
